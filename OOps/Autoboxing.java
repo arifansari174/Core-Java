7ecc891850b0f4c91bb73c60738f6e09671f9791
Arif Ansari <arifansari63915@gmail.com>
2025-09-20 23:52:12 +0530

    Add files via upload

---
+++ OOps/Autoboxing.java
@@ -0,0 +1,13 @@
+public class AutoBoxing {
+	public static void main(String[] args) {
+		int a=10;
+		Integer b = 20;
+		System.out.println(b);
+		
+		char ch1 = 'a';
+		Character ch2=ch1;
+		System.out.println(ch2);
+		
+	}
+
+}
---
+++ OOps/Unboxing.java
@@ -0,0 +1,13 @@
+public class Unboxing {
+	public static void main(String[] args) {
+		
+		Integer a=10;
+		int b = a;
+		System.out.println(b);
+		
+		Character ch1='a';
+		char ch2=ch1;
+		System.out.println(ch2);
+	}
+
+}
